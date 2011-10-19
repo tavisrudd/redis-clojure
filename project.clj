115454c619b7216cf85245bb4854d3bd7634e610
Tavis Rudd <tavis@damnsimple.com>
2011-10-18 18:54:38 -0700

    update clojars name and url to github

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject org.clojars.nathell/redis-clojure "1.3.0"
+(defproject org.clojars.tavisrudd/redis-clojure "1.3.0"
   :description "A client library for Redis"
-  :url "http://github.com/ragnard/redis-clojure"
+  :url "http://github.com/tavisrudd/redis-clojure"
   :dependencies [
                  [org.clojure/clojure "1.3.0"]
-                 ;[org.clojure.contrib/ns-utils "1.3.0-alpha4"]
-
                  [commons-pool/commons-pool "1.5.5"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :warn-on-reflection true)
