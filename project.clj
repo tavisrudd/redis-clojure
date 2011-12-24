c2697163980ef3be4993c79d1b398197945e5c67
Tavis Rudd <tavis@damnsimple.com>
2011-12-24 08:47:09 -0800

    bump version

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject org.clojars.tavisrudd/redis-clojure "1.3.0"
+(defproject org.clojars.tavisrudd/redis-clojure "1.3.1-SNAPSHOT"
   :description "A client library for Redis"
   :url "http://github.com/tavisrudd/redis-clojure"
   :dependencies [
                  [org.clojure/clojure "1.3.0"]
                  [commons-pool/commons-pool "1.5.5"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :warn-on-reflection true)
